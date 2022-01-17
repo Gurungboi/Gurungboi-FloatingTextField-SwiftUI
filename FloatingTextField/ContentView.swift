d639c601fbdf00eac49f53c1f7daedd15294f7d0
Sunil Gurung <sunil@prezzee.com.au>
2022-01-17 16:41:42 +1100

    Initial Commit

---
+++ FloatingTextField/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FloatingTextField
+//
+//  Created by Sunil on 17/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FloatingTextField/FloatingTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTextFieldApp.swift
+//  FloatingTextField
+//
+//  Created by Sunil on 17/1/22.
+//
+
+import SwiftUI
+
+@main
+struct FloatingTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
